9d547aead056004647845b640ba24cdec96c9adb
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-11-28 12:25:44 +0100

    MOB-407 Unused fromjson deleted

--- lib/data/event/model/event_attendees_data_model.dart
+++ lib/data/event/model/event_attendees_data_model.dart
@@ -7,12 +7,4 @@ class EventAttendeesDataModel {
     required this.users,
   });
 
-  factory EventAttendeesDataModel.fromJson(Map<String, dynamic> json) {
-    return EventAttendeesDataModel(
-      totalUsers: json["totalUsers"] ?? 0,
-      users: (json["users"] as List<dynamic>? ?? [])
-          .map((e) => e.toString())
-          .toList(),
-    );
-  }
 }
