dcba5e6261857dbd0a0a0d9cc56a780d7985f046
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-11-28 12:50:02 +0100

    MOB-407 Remove fromJson mapper from EventAttendees

--- lib/domain/event/model/event_attendees.dart
+++ lib/domain/event/model/event_attendees.dart
@@ -5,16 +5,4 @@ class EventAttendees {
   final List<EventAttendeeRemoteModel> users;
 
   EventAttendees({required this.totalUsers, required this.users});
-
-  factory EventAttendees.fromJson(Map<String, dynamic> json) {
-    final attendeesJson = (json['users'] as List<dynamic>?) ?? [];
-    final attendees = attendeesJson
-        .map((e) => EventAttendeeRemoteModel.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    return EventAttendees(
-      totalUsers: attendees.length,
-      users: attendees,
-    );
-  }
 }
